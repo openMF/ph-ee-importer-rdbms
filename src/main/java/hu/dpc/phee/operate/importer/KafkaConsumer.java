66808df45830203937928925bedbcc01fdea5203
Kristof Jozsa <kristof.jozsa@gmail.com>
2020-04-07 17:03:28 +0200

    refactor, cleanup

--- src/main/java/hu/dpc/phee/operate/importer/KafkaConsumer.java
+++ src/main/java/hu/dpc/phee/operate/importer/KafkaConsumer.java
@@ -1,12 +1,6 @@
 package hu.dpc.phee.operate.importer;
 
 import com.jayway.jsonpath.DocumentContext;
-import hu.dpc.phee.operate.importer.persistence.Task;
-import hu.dpc.phee.operate.importer.persistence.TaskRepository;
-import hu.dpc.phee.operate.importer.persistence.Transaction;
-import hu.dpc.phee.operate.importer.persistence.TransactionRepository;
-import hu.dpc.phee.operate.importer.persistence.Variable;
-import hu.dpc.phee.operate.importer.persistence.VariableRepository;
 import org.apache.kafka.common.TopicPartition;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -16,10 +10,7 @@ import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.listener.ConsumerSeekAware;
 import org.springframework.stereotype.Component;
 
-import java.util.Arrays;
-import java.util.List;
 import java.util.Map;
-import java.util.Set;
 
 @Component
 public class KafkaConsumer implements ConsumerSeekAware {
@@ -35,13 +26,7 @@ public class KafkaConsumer implements ConsumerSeekAware {
     private JsonParser jsonParser;
 
     @Autowired
-    private TaskRepository taskRepository;
-
-    @Autowired
-    private VariableRepository variableRepository;
-
-    @Autowired
-    private TransactionRepository transactionRepository;
+    private RecordParser recordParser;
 
     @KafkaListener(topics = "${importer.kafka.topic}")
     public void listen(String rawData) {
@@ -49,49 +34,12 @@ public class KafkaConsumer implements ConsumerSeekAware {
         logger.debug("from kafka: {}", json.jsonString());
 
         String valueType = json.read("$.valueType");
-        Long workflowInstanceKey = json.read("$.value.workflowInstanceKey");
-        Long timestamp = json.read("$.timestamp");
         if ("VARIABLE".equals(valueType)) {
-            Variable variable = new Variable();
-            variable.setWorkflowInstanceKey(workflowInstanceKey);
-            variable.setWorkflowKey(json.read("$.value.workflowKey"));
-            variable.setTimestamp(timestamp);
-            String name = json.read("$.value.name");
-            variable.setName(name);
-            String value = json.read("$.value.value");
-            variable.setValue(value);
+            recordParser.parseVariable(json);
 
-            List<String> businessIds = Arrays.asList("transactionId", "partyId");
-            if (businessIds.contains(name)) {
-                Transaction transaction = new Transaction();
-                transaction.setTimestamp(timestamp);
-                transaction.setWorkflowInstanceKey(workflowInstanceKey);
-                transaction.setBusinessKey(value);
-                transaction.setBusinessKeyType(name);
-                transactionRepository.save(transaction);
-            }
-            variableRepository.save(variable);
-            return;
+        } else {
+            recordParser.parseTask(json);
         }
-
-        String intent = json.read("$.intent");
-        String recordType = json.read("$.recordType");
-        String type = json.read("$.value.type");
-
-        if (type == null) {
-            return;
-        }
-
-        logger.info("{} {} {}", intent, recordType, type);
-
-        Task task = new Task();
-        task.setWorkflowInstanceKey(workflowInstanceKey);
-        task.setWorkflowKey(json.read("$.value.workflowKey"));
-        task.setTimestamp(timestamp);
-        task.setIntent(intent);
-        task.setRecordType(recordType);
-        task.setType(type);
-        taskRepository.save(task);
     }
 
     @Override
---
+++ src/main/java/hu/dpc/phee/operate/importer/RecordParser.java
@@ -0,0 +1,76 @@
+package hu.dpc.phee.operate.importer;
+
+import com.jayway.jsonpath.DocumentContext;
+import hu.dpc.phee.operate.importer.persistence.Task;
+import hu.dpc.phee.operate.importer.persistence.TaskRepository;
+import hu.dpc.phee.operate.importer.persistence.Transaction;
+import hu.dpc.phee.operate.importer.persistence.TransactionRepository;
+import hu.dpc.phee.operate.importer.persistence.Variable;
+import hu.dpc.phee.operate.importer.persistence.VariableRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Component
+public class RecordParser {
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @Autowired
+    private TaskRepository taskRepository;
+
+    @Autowired
+    private VariableRepository variableRepository;
+
+    @Autowired
+    private TransactionRepository transactionRepository;
+
+    public void parseTask(DocumentContext json) {
+        String type = json.read("$.value.type");
+        if (type == null) {
+            return;
+        }
+
+        String intent = json.read("$.intent");
+        String recordType = json.read("$.recordType");
+        logger.info("{} {} {}", intent, recordType, type);
+
+        Task task = new Task();
+        task.setWorkflowInstanceKey(json.read("$.value.workflowInstanceKey"));
+        task.setWorkflowKey(json.read("$.value.workflowKey"));
+        task.setTimestamp(json.read("$.timestamp"));
+        task.setIntent(intent);
+        task.setRecordType(recordType);
+        task.setType(type);
+        taskRepository.save(task);
+    }
+
+    public void parseVariable(DocumentContext json) {
+        Long workflowInstanceKey = json.read("$.value.workflowInstanceKey");
+        Long timestamp = json.read("$.timestamp");
+
+        Variable variable = new Variable();
+        variable.setWorkflowInstanceKey(workflowInstanceKey);
+        variable.setWorkflowKey(json.read("$.value.workflowKey"));
+        variable.setTimestamp(timestamp);
+        String name = json.read("$.value.name");
+        variable.setName(name);
+        String value = json.read("$.value.value");
+        variable.setValue(value);
+
+        List<String> businessIds = Arrays.asList("transactionId", "partyId");
+        if (businessIds.contains(name)) {
+            Transaction transaction = new Transaction();
+            transaction.setTimestamp(timestamp);
+            transaction.setWorkflowInstanceKey(workflowInstanceKey);
+            transaction.setBusinessKey(value);
+            transaction.setBusinessKeyType(name);
+            transactionRepository.save(transaction);
+        }
+        variableRepository.save(variable);
+    }
+
+}
