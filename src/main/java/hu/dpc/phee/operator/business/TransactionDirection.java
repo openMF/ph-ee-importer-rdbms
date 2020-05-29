74f887f8220649a7a23ea2903df9d6b9c4baca3c
Zoltan Nebli <zoltan.nebli@dpc.hu>
2020-05-29 15:03:03 +0200

    changed direction parsing

--- src/main/java/hu/dpc/phee/operator/business/TransactionDirection.java
+++ src/main/java/hu/dpc/phee/operator/business/TransactionDirection.java
@@ -1,24 +1,30 @@
 package hu.dpc.phee.operator.business;
 
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 public enum TransactionDirection {
-    INCOMING("payee"),
-    OUTGOING("payer"),
-    IGNORED("ignored");
+    INCOMING(Arrays.asList("PayeeQuoteTransfer", "PayerTransactionRequest")),
+    OUTGOING(Arrays.asList("PayerFundTransfer", "PayeeTransactionRequest")),
+    IGNORED(Collections.emptyList());
 
-    private String prefix;
+    private List<String> processes;
 
-    TransactionDirection(String prefix) {
-        this.prefix = prefix;
+    TransactionDirection(List<String> processes) {
+        this.processes = processes;
+    }
+
+    public List<String> getProcesses() {
+        return processes;
     }
 
     public static TransactionDirection fromBpmn(String processId) {
         if(processId != null) {
+            String process = processId.split("-")[0];
             return Arrays.stream(TransactionDirection.values())
-                    .filter(d -> processId.toLowerCase().startsWith(d.prefix))
-                    .findFirst()
-                    .orElse(IGNORED);
+                    .filter(d -> d.getProcesses().contains(process))
+                    .findFirst().orElse(IGNORED);
         } else {
             return IGNORED;
         }
