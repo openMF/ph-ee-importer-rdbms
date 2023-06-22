99c89e0ef01769208c2a60488495b9d7abb69710
Kristof Jozsa <kristof.jozsa@gmail.com>
2023-06-22 17:29:23 +0200

    find bpmnProcessId even from other locations

--- src/main/java/hu/dpc/phee/operator/streams/EventParser.java
+++ src/main/java/hu/dpc/phee/operator/streams/EventParser.java
@@ -51,12 +51,27 @@ public class EventParser {
 
 
     public Pair<String, String> retrieveTenant(DocumentContext record) {
-        String bpmnProcessIdWithTenant = record.read("$.value.bpmnProcessId", String.class);
+        String bpmnProcessIdWithTenant = findBpmnProcessId(record);
+
         String[] split = bpmnProcessIdWithTenant.split("-");
         if (split.length < 2) {
             throw new RuntimeException("Invalid bpmnProcessId, has no tenant information: '" + bpmnProcessIdWithTenant + "'");
         }
         return Pair.of(split[0], split[1]);
+    }
+
+    private String findBpmnProcessId(DocumentContext record) {
+        String bpmnProcessIdWithTenant = record.read("$.value.bpmnProcessId", String.class);
+        if (bpmnProcessIdWithTenant == null) {
+            logger.warn("can't find bpmnProcessId in record: {}, trying alternative ways..", record.jsonString());
+            List<String> ids = record.read("$.value..bpmnProcessId", List.class);
+            if (ids.size() > 1) {
+                throw new RuntimeException("Invalid bpmnProcessIdWithTenant, has more than one bpmnProcessIds: '" + ids + "'");
+            }
+            bpmnProcessIdWithTenant = ids.get(0);
+        }
+        logger.debug("resolved bpmnProcessIdWithTenant: {}", bpmnProcessIdWithTenant);
+        return bpmnProcessIdWithTenant;
     }
 
     public Transfer retrieveOrCreateTransfer(String bpmn, DocumentContext record) {
