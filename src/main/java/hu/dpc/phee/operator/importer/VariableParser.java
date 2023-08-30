6390c85c19e9050611f19c27e2375788bda22b5c
Danish Jamal <31315800+danishjamal104@users.noreply.github.com>
2023-08-30 16:33:13 +0530

    GOV-223 Added logic to parse registeringInstituteId, payerIdentifier & clientCorrelationId (#47)
    * GOV-223 Added logic to parse registeringInstituteId, payerIdentifier, clientCorrelationId
    
    * Added approved count and amount, new field in batch table/dto
    
    * Batch DAO bug fixed

--- src/main/java/hu/dpc/phee/operator/importer/VariableParser.java
+++ src/main/java/hu/dpc/phee/operator/importer/VariableParser.java
@@ -94,6 +94,9 @@ public class VariableParser {
         batchParsers.put("completedAmount", pair -> pair.getFirst().setCompletedAmount(Long.parseLong(pair.getSecond())));
         batchParsers.put("resultFile", pair -> pair.getFirst().setResult_file(strip(pair.getSecond())));
         batchParsers.put("paymentMode", pair -> pair.getFirst().setPaymentMode(strip(pair.getSecond())));
+        batchParsers.put("registeringInstituteId", pair -> pair.getFirst().setRegisteringInstitutionId(pair.getSecond()));
+        batchParsers.put("payerIdentifier", pair -> pair.getFirst().setPayerFsp(pair.getSecond()));
+        batchParsers.put("clientCorrelationId", pair -> pair.getFirst().setCorrelationId(pair.getSecond()));
 
         outboundMessageParsers.put("tenantId",pair -> pair.getFirst().setTenantId(Long.parseLong(pair.getSecond())));
         outboundMessageParsers.put("externalId",pair -> pair.getFirst().setExternalId(strip(pair.getSecond())));
