c84438964729bbdb5b634dae03943446486e8e74
Kristof Jozsa <kristof.jozsa@gmail.com>
2023-10-09 14:54:21 +0200

    add MSK client

--- src/main/java/hu/dpc/phee/operator/config/KafkaConfiguration.java
+++ src/main/java/hu/dpc/phee/operator/config/KafkaConfiguration.java
@@ -1,8 +1,11 @@
 package hu.dpc.phee.operator.config;
 
+import org.apache.kafka.clients.CommonClientConfigs;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.config.SaslConfigs;
 import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.common.serialization.StringDeserializer;
+import org.jetbrains.annotations.NotNull;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
@@ -39,45 +42,63 @@ public class KafkaConfiguration {
     @Value("kafka.consumer-group")
     private String consumerGroup;
 
-    @Bean
-    KafkaListenerContainerFactory<ConcurrentMessageListenerContainer<String, String>> kafkaListenerContainerFactory() {
-        ConcurrentKafkaListenerContainerFactory<String, String> factory = new ConcurrentKafkaListenerContainerFactory<>();
-        factory.setConsumerFactory(consumerFactory());
-        factory.setConcurrency(3);  // TODO externalize
-        factory.getContainerProperties().setPollTimeout(3000); // TODO externalize
+    @Value("${kafka.msk}")
+    private boolean msk;
 
-        return factory;
-    }
+    @Value("${kafka.commit-interval-ms}")
+    private int commitIntervalMs;
+
+//
+//    @Bean
+//    public KafkaListenerContainerFactory<ConcurrentMessageListenerContainer<String, String>> kafkaListenerContainerFactory() {
+//        ConcurrentKafkaListenerContainerFactory<String, String> factory = new ConcurrentKafkaListenerContainerFactory<>();
+//        factory.setConsumerFactory(consumerFactory());
+//        factory.setConcurrency(3);  // TODO externalize
+//        factory.getContainerProperties().setPollTimeout(3000); // TODO externalize
+//        return factory;
+//    }
 
     @Bean
     public ConsumerFactory<String, String> consumerFactory() {
-        return new DefaultKafkaConsumerFactory<>(consumerConfigs());
-    }
-
-    @Bean(name = KafkaStreamsDefaultConfiguration.DEFAULT_STREAMS_CONFIG_BEAN_NAME)
-    KafkaStreamsConfiguration kStreamsConfig() {
-        Map<String, Object> props = new HashMap<>();
-        props.put(APPLICATION_ID_CONFIG, consumerGroup);
-        props.put(BOOTSTRAP_SERVERS_CONFIG, kafkaBrokers);
-        props.put(DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass().getName());
-        props.put(COMMIT_INTERVAL_MS_CONFIG, 1000);
-        return new KafkaStreamsConfiguration(props);
-    }
-
-    public Map<String, Object> consumerConfigs() {
         String hostname = buildKafkaClientId(logger);
-        logger.info("configuring Kafka consumer {} for bootstrap servers {}", hostname, kafkaBrokers);
+        logger.info("configuring Kafka consumer '{}' for bootstrap servers: {}", hostname, kafkaBrokers);
 
-        Map<String, Object> properties = new HashMap<>();
-        properties.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaBrokers);
+        Map<String, Object> properties = commonProperties();
         properties.put(ConsumerConfig.CLIENT_ID_CONFIG, hostname);
         properties.put(ConsumerConfig.GROUP_ID_CONFIG, hostname);
         properties.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
         properties.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+
+        return new DefaultKafkaConsumerFactory<>(properties);
+    }
+
+    private Map<String, Object> commonProperties() {
+        Map<String, Object> properties = new HashMap<>();
+        properties.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaBrokers);
+
+        if (msk) {
+            logger.info("configuring Kafka client with AWS MSK support");
+            properties.put(CommonClientConfigs.SECURITY_PROTOCOL_CONFIG, "SASL_SSL");
+            properties.put(SaslConfigs.SASL_MECHANISM, "AWS_MSK_IAM");
+            properties.put(SaslConfigs.SASL_JAAS_CONFIG, "software.amazon.msk.auth.iam.IAMLoginModule required;");
+            properties.put(SaslConfigs.SASL_CLIENT_CALLBACK_HANDLER_CLASS, "software.amazon.msk.auth.iam.IAMClientCallbackHandler");
+        }
+
         return properties;
     }
 
-    public static String buildKafkaClientId(Logger logger) {
+    @Bean(name = KafkaStreamsDefaultConfiguration.DEFAULT_STREAMS_CONFIG_BEAN_NAME)
+    public KafkaStreamsConfiguration kStreamsConfig() {
+        logger.info("configuring Kafka streams for consumer group '{}' for bootstrap servers: {}", consumerGroup, kafkaBrokers);
+
+        Map<String, Object> properties = commonProperties();
+        properties.put(APPLICATION_ID_CONFIG, consumerGroup);
+        properties.put(DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.StringSerde.class.getName());
+        properties.put(COMMIT_INTERVAL_MS_CONFIG, commitIntervalMs);
+        return new KafkaStreamsConfiguration(properties);
+    }
+
+    private static String buildKafkaClientId(Logger logger) {
         String clientId;
         try {
             clientId = InetAddress.getLocalHost().getHostName();
