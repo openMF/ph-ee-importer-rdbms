9638755fbb08ec9bbddba7790b58cec01d4aa4a4
Kristof Jozsa <kristof.jozsa@gmail.com>
2023-11-17 10:39:08 +0100

    escape variable values as json

--- src/main/java/hu/dpc/phee/operator/streams/EventParser.java
+++ src/main/java/hu/dpc/phee/operator/streams/EventParser.java
@@ -241,7 +241,7 @@ public class EventParser {
                                 .withName(variableName)
                                 .withWorkflowKey(workflowKey)
                                 .withTimestamp(timestamp)
-                                .withValue(value));
+                                .withValue(StringEscapeUtils.escapeJson(value)));
             }
 
             case "INCIDENT" -> {
