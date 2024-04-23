1e3ceb75bdfc77cc3710150e173c594ab0e867cf
Kristof Jozsa <kristof.jozsa@gmail.com>
2024-04-23 14:36:19 +0200

    simplify

--- src/main/java/hu/dpc/phee/operator/entity/transfer/TransferRepository.java
+++ src/main/java/hu/dpc/phee/operator/entity/transfer/TransferRepository.java
@@ -1,12 +1,12 @@
 package hu.dpc.phee.operator.entity.transfer;
 
-import jakarta.persistence.EntityManager;
-import jakarta.persistence.PersistenceContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 public interface TransferRepository extends JpaRepository<Transfer, Long>, JpaSpecificationExecutor {
     Logger logger = LoggerFactory.getLogger(TransferRepository.class);
@@ -16,17 +16,12 @@ public interface TransferRepository extends JpaRepository<Transfer, Long>, JpaSp
 
     @Transactional
     default Transfer saveIfFresh(Transfer transfer) {
-        EntityManager entityManager = entityManager();
-
-        Transfer existingTransfer = entityManager.find(Transfer.class, transfer.getWorkflowInstanceKey());
-        if (existingTransfer == null || existingTransfer.getLastUpdated() < transfer.getLastUpdated()) {
-            return entityManager.merge(transfer);
+        Optional<Transfer> existingTransfer = findById(transfer.getWorkflowInstanceKey());
+        if (existingTransfer.isEmpty() || existingTransfer.get().getLastUpdated() < transfer.getLastUpdated()) {
+            return save(transfer);
         } else {
             logger.warn("not merging obsolete transfer: {}", transfer);
-            return existingTransfer;
+            return existingTransfer.get();
         }
     }
-
-    @PersistenceContext
-    EntityManager entityManager();
 }
--- src/main/java/hu/dpc/phee/operator/entity/variable/VariableRepository.java
+++ src/main/java/hu/dpc/phee/operator/entity/variable/VariableRepository.java
@@ -1,34 +1,30 @@
 package hu.dpc.phee.operator.entity.variable;
 
-import jakarta.persistence.EntityManager;
-import jakarta.persistence.PersistenceContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface VariableRepository extends CrudRepository<Variable, Long> {
     Logger logger = LoggerFactory.getLogger(VariableRepository.class);
 
     List<Variable> findByWorkflowInstanceKey(Long workflowInstanceKey);
 
+    Optional<Variable> findByWorkflowInstanceKeyAndName(Long workflowInstanceKey, String name);
+
 
     @Transactional
     default Variable saveIfFresh(Variable variable) {
-        EntityManager entityManager = entityManager();
-        VariableId id = new VariableId(variable.getWorkflowInstanceKey(), variable.getName());
+        Optional<Variable> existingVariable = findByWorkflowInstanceKeyAndName(variable.getWorkflowInstanceKey(), variable.getName());
 
-        Variable existingVariable = entityManager.find(Variable.class, id);
-        if (existingVariable == null || existingVariable.getTimestamp() < variable.getTimestamp()) {
-            return entityManager.merge(variable);
+        if (existingVariable.isEmpty() || existingVariable.get().getTimestamp() < variable.getTimestamp()) {
+            return save(variable);
         } else {
-            logger.warn("not merging obsolete variable: {}", id);
-            return existingVariable;
+            logger.warn("not merging obsolete variable: {} for workflow instance: {}", variable.getName(), variable.getWorkflowInstanceKey());
+            return existingVariable.orElse(null);
         }
     }
-
-    @PersistenceContext
-    EntityManager entityManager();
 }
