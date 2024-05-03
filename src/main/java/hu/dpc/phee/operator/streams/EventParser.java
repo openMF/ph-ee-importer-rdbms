9fbc565bdfad9aafa4a86869276a0abc68fc0bf2
Kristof Jozsa <kristof.jozsa@gmail.com>
2024-05-03 14:44:01 +0200

    fix long/integer #2

--- src/main/java/hu/dpc/phee/operator/streams/EventParser.java
+++ src/main/java/hu/dpc/phee/operator/streams/EventParser.java
@@ -118,7 +118,7 @@ public class EventParser {
 
         Long workflowKey = record.read("$.value.processDefinitionKey");
         Long workflowInstanceKey = record.read("$.value.processInstanceKey");
-        Long position = Long.valueOf(record.read("$.position", Integer.class));
+        Long position = record.read("$.position", Long.class);
         Long timestamp = record.read("$.timestamp");
         String bpmnElementType = record.read("$.value.bpmnElementType");
         String bpmnEventType = record.read("$.value.bpmnEventType");
