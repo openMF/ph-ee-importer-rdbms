72e18a50f5fea154d49b97d4bc0e7c24d50cf251
GeluHUN <peter.gelei@dpc.hu>
2024-07-23 18:37:51 +0200

    create FileTransportConfig

---
+++ src/main/java/hu/dpc/phee/operator/config/FileTransportTransformerConfig.java
@@ -0,0 +1,32 @@
+package hu.dpc.phee.operator.config;
+
+import hu.dpc.phee.operator.config.transformer.Flow;
+import jakarta.annotation.PostConstruct;
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@EnableConfigurationProperties
+@Configuration
+@ConfigurationProperties(prefix = "file-transport")
+@Data
+@Slf4j
+public class FileTransportTransformerConfig {
+
+    private final List<Flow> flows = new ArrayList<>();
+
+    @PostConstruct
+    public void setup() {
+        log.info("loaded FileTransport transformers for {} flows from configuration", flows.size());
+    }
+
+    public Optional<Flow> findFlow(String name) {
+        return flows.stream().filter(flow -> name.equalsIgnoreCase(flow.getName())).findAny();
+    }
+}
