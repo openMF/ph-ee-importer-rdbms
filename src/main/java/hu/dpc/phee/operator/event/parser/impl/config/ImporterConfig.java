b2ddcf4ffa834a8c88639452071e88990cd05944
GeluHUN <peter.gelei@dpc.hu>
2024-07-30 08:20:24 +0200

    fix bad config

--- src/main/java/hu/dpc/phee/operator/event/parser/impl/config/ImporterConfig.java
+++ src/main/java/hu/dpc/phee/operator/event/parser/impl/config/ImporterConfig.java
@@ -1,12 +1,20 @@
 package hu.dpc.phee.operator.event.parser.impl.config;
 
+import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @EnableConfigurationProperties
 @ConfigurationProperties(prefix = "importer")
-public record ImporterConfig(Kafka kafka) {
+@Data
+public class ImporterConfig {
 
-    public record Kafka(String topic, int aggregationWindowSeconds, int aggregationAfterEndSeconds) {
+    private Kafka kafka;
+
+    @Data
+    public static class Kafka {
+        String topic;
+        int aggregationWindowSeconds;
+        int aggregationAfterEndSeconds;
     }
 }
