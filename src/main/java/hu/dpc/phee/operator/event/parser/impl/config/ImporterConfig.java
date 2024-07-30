2dff34c9acda34db944aa00c97320a8210383101
GeluHUN <peter.gelei@dpc.hu>
2024-07-30 07:53:39 +0200

    fix missing config annotation

--- src/main/java/hu/dpc/phee/operator/event/parser/impl/config/ImporterConfig.java
+++ src/main/java/hu/dpc/phee/operator/event/parser/impl/config/ImporterConfig.java
@@ -1,7 +1,11 @@
 package hu.dpc.phee.operator.event.parser.impl.config;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
 
+@EnableConfigurationProperties
+@Configuration
 @ConfigurationProperties(prefix = "importer")
 public record ImporterConfig(Kafka kafka) {
 
