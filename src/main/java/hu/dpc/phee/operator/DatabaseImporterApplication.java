b3b12e391cac636c50847875e0b203451c99aacf
GeluHUN <peter.gelei@dpc.hu>
2024-07-30 09:05:15 +0200

    fix bad config

--- src/main/java/hu/dpc/phee/operator/DatabaseImporterApplication.java
+++ src/main/java/hu/dpc/phee/operator/DatabaseImporterApplication.java
@@ -1,20 +1,15 @@
 package hu.dpc.phee.operator;
 
 import com.fasterxml.jackson.dataformat.csv.CsvMapper;
-import hu.dpc.phee.operator.event.parser.impl.transfer.config.TransferTransformerConfig;
-import hu.dpc.phee.operator.event.parser.impl.transport.config.FileTransportTransformerConfig;
-import hu.dpc.phee.operator.streams.config.ImporterConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, DataSourceTransactionManagerAutoConfiguration.class})
 @Configuration
-@EnableConfigurationProperties(value = {TransferTransformerConfig.class, FileTransportTransformerConfig.class, ImporterConfig.class})
 public class DatabaseImporterApplication {
 
     static {
--- src/main/java/hu/dpc/phee/operator/streams/config/ImporterConfig.java
+++ src/main/java/hu/dpc/phee/operator/streams/config/ImporterConfig.java
@@ -3,9 +3,7 @@ package hu.dpc.phee.operator.streams.config;
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.context.annotation.Configuration;
 
-@Configuration
 @EnableConfigurationProperties
 @ConfigurationProperties(prefix = "importer")
 @Data
